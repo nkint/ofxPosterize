e6958ff539eafb344b0ade5b41c67427657bef9a
alberto <alberto@albertos-MacBook-Pro.local>
2013-02-15 13:13:25 +0100

    first commit

---
+++ example_extractColor/src/testApp.h
@@ -0,0 +1,48 @@
+#ifndef _TEST_APP
+#define _TEST_APP
+
+#include "ofMain.h"
+
+#include <iostream>
+#include <vector>
+#include <GLUT/GLUT.h>
+
+#include "ofxPosterize.h"
+
+
+class testApp : public ofSimpleApp{
+    
+public:
+    
+	void setup();
+	void update();
+	void draw();
+    
+	void keyPressed  (int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased();
+    
+  	void dragEvent(ofDragInfo info);
+    
+private:
+    void extractPalette(string filename);
+    void writeWebInterface();
+    
+    ofImage source;
+    bool ready;
+    
+    ofImage drawPalette(map<int, int> histogram, int width, int height);
+    
+    ofImage average;
+    ofImage averagePalette;
+    map<int, int> averageHistogram;
+    
+    ofImage clustered;
+    ofImage clusteredPalette;
+	map<int, int> clusteredHistogram;
+    
+};
+
+#endif
